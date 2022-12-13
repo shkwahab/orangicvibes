92abbc70edc6fe40c1a83804b2ee0cb3e8e26e86
shkwahab <wahab209266@gmail.com>
2022-12-14 00:46:16 +0500

    food

---
+++ src/components/Navbar.js
@@ -0,0 +1,73 @@
+import React from 'react'
+import { IoIosArrowDown } from "react-icons/io"
+import { BiSearch,BiUser } from "react-icons/bi"
+import { FiHeart } from "react-icons/fi"
+import { HiOutlineShoppingBag } from "react-icons/hi"
+import {HiBars3} from "react-icons/hi2"
+const Navbar = (props) => {
+    return (
+        <>
+            <section id="nav">
+                <div className="py-4 mx-4 lg:mx-20  flex  justify-between items-center">
+                    <div className=" items-center  lg:hidden flex flex-1">
+                        <HiBars3 className=' text-3xl  text-blue-900' />
+                    </div>
+                    <div className="  flex-1 sm:text-2xl font-semibold  text-[#96AE01]" id="logo">
+                        {props.sitetitle}
+                    </div>
+
+                    <div className='lg:block hidden'>
+                        <ul className="justify-center   lg:flex  font-medium text-gray-600 ">
+                            <div className='mx-3 sm:flex items-end  gap-1 hover:text-[#96AE01] cursor-pointer transition-all duration-300'>
+                                <li  className=" text-sm">{props.category1}</li>
+                            </div>
+                            <div className='mx-3 sm:flex lg:items-end   gap-1 hover:text-[#96AE01] cursor-pointer transition-all duration-300'>
+                                <li  className=" text-sm">{props.category2}</li>
+                                <IoIosArrowDown className=' text-sm ' />
+                            </div>
+                            <div className='mx-3 sm:flex lg:items-end   gap-1 hover:text-[#96AE01] cursor-pointer transition-all duration-300'>
+                                <li  className=" text-sm">{props.category3}</li>
+                                <IoIosArrowDown className=' text-sm ' />
+                            </div>
+                            <div className='mx-3 lg:flex lg:items-end   gap-1 hover:text-[#96AE01] cursor-pointer transition-all duration-300'>
+                                <li  className=" text-sm">{props.category4}</li>
+                                <IoIosArrowDown className=' text-sm ' />
+                            </div>
+                            <div className='mx-3 lg:flex lg:items-end   gap-1 hover:text-[#96AE01] cursor-pointer transition-all duration-300'>
+                                <li  className=" text-sm">{props.category5}</li>
+
+                            </div>
+                            <div className='mx-3 lg:flex lg:items-end   gap-1 hover:text-[#96AE01] cursor-pointer transition-all duration-300'>
+                                <li  className=" text-sm">{props.category6}</li>
+
+                            </div>
+
+                        </ul>
+                    </div>
+
+                    <div className=" lg:flex-1 flex  justify-end">
+                        <div className="flex">
+                            <div className="bg-[#F5F2FF] sm:block hidden p-2 md:p-3 rounded-full cursor-pointer">
+                                <BiSearch className=' text-xs sm:text-base text-gray-700' />
+                            </div>
+                            <div className="bg-[#FFF3EC] sm:block hidden p-2 md:p-3 rounded-full mx-4 cursor-pointer">
+                                <BiUser className=' text-xs sm:text-base text-gray-800' />
+                            </div>
+                            <div className="bg-[#ECFBFF] sm:block hidden p-2 md:p-3 pt-[14px] rounded-full cursor-pointer">
+                                <FiHeart className=' text-xs sm:text-base text-gray-700' />
+                            </div>
+                            <div className="bg-[#FEEFD0] p-3 md:p-3 pt-[14px] rounded-full cursor-pointer mx-4">
+                                <HiOutlineShoppingBag className='text-xs sm:text-base text-gray-700' />
+                            </div>
+                        </div>
+                    </div>
+
+                </div>
+
+                <hr className="bg-black h-[2px] opacity-10" />
+            </section>
+        </>
+    )
+}
+
+export default Navbar
