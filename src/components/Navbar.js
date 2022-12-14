bcbbe7ceebfc0481639fa01f5e653d10e8a90284
shkwahab <wahab209266@gmail.com>
2022-12-14 12:08:12 +0500

    food

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,10 +1,17 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { IoIosArrowDown } from "react-icons/io"
-import { BiSearch,BiUser } from "react-icons/bi"
+import { BiSearch, BiUser } from "react-icons/bi"
 import { FiHeart } from "react-icons/fi"
 import { HiOutlineShoppingBag } from "react-icons/hi"
-import {HiBars3} from "react-icons/hi2"
+import { HiBars3 } from "react-icons/hi2"
 const Navbar = (props) => {
+    const [isShopSlider, SetShopSlider] = useState(true)
+    const ShopSlider = () => {
+        SetShopSlider(!isShopSlider);
+    }
+    const ShopSliderClose = () => {
+        SetShopSlider(!isShopSlider);
+    }
     return (
         <>
             <section id="nav">
@@ -19,44 +26,43 @@ const Navbar = (props) => {
                     <div className='lg:block hidden'>
                         <ul className="justify-center   lg:flex  font-medium text-gray-600 ">
                             <div className='mx-3 sm:flex items-end  gap-1 hover:text-[#96AE01] cursor-pointer transition-all duration-300'>
-                                <li  className=" text-sm">{props.category1}</li>
+                                <li className=" text-sm">{props.category1}</li>
+                            </div>
+                            <div onMouseEnter={ShopSlider} onMouseLeave={ShopSliderClose}  className='mx-3 sm:flex lg:items-end   gap-1 hover:text-[#96AE01] cursor-pointer transition-all duration-300'>
+                                <li className=" text-sm">{props.category2}</li>
+                                <IoIosArrowDown className=' text-sm ' />
+
                             </div>
                             <div className='mx-3 sm:flex lg:items-end   gap-1 hover:text-[#96AE01] cursor-pointer transition-all duration-300'>
-                                <li  className=" text-sm">{props.category2}</li>
-                                <IoIosArrowDown className=' text-sm ' />
-                            </div>
-                            <div className='mx-3 sm:flex lg:items-end   gap-1 hover:text-[#96AE01] cursor-pointer transition-all duration-300'>
-                                <li  className=" text-sm">{props.category3}</li>
+                                <li className=" text-sm">{props.category3}</li>
                                 <IoIosArrowDown className=' text-sm ' />
                             </div>
                             <div className='mx-3 lg:flex lg:items-end   gap-1 hover:text-[#96AE01] cursor-pointer transition-all duration-300'>
-                                <li  className=" text-sm">{props.category4}</li>
+                                <li className=" text-sm">{props.category4}</li>
                                 <IoIosArrowDown className=' text-sm ' />
                             </div>
                             <div className='mx-3 lg:flex lg:items-end   gap-1 hover:text-[#96AE01] cursor-pointer transition-all duration-300'>
-                                <li  className=" text-sm">{props.category5}</li>
+                                <li className=" text-sm">{props.category5}</li>
 
                             </div>
                             <div className='mx-3 lg:flex lg:items-end   gap-1 hover:text-[#96AE01] cursor-pointer transition-all duration-300'>
-                                <li  className=" text-sm">{props.category6}</li>
-
+                                <li className=" text-sm">{props.category6}</li>
                             </div>
-
                         </ul>
                     </div>
 
                     <div className=" lg:flex-1 flex  justify-end">
                         <div className="flex">
-                            <div className="bg-[#F5F2FF] sm:block hidden p-2 md:p-3 rounded-full cursor-pointer">
+                            <div className="bg-[#F5F2FF] sm:block hidden p-3 rounded-full cursor-pointer">
                                 <BiSearch className=' text-xs sm:text-base text-gray-700' />
                             </div>
-                            <div className="bg-[#FFF3EC] sm:block hidden p-2 md:p-3 rounded-full mx-4 cursor-pointer">
+                            <div className="bg-[#FFF3EC] sm:block hidden p-3 rounded-full mx-4 cursor-pointer">
                                 <BiUser className=' text-xs sm:text-base text-gray-800' />
                             </div>
-                            <div className="bg-[#ECFBFF] sm:block hidden p-2 md:p-3 pt-[14px] rounded-full cursor-pointer">
+                            <div className="bg-[#ECFBFF] sm:block hidden p-3 pt-[14px] rounded-full cursor-pointer">
                                 <FiHeart className=' text-xs sm:text-base text-gray-700' />
                             </div>
-                            <div className="bg-[#FEEFD0] p-3 md:p-3 pt-[14px] rounded-full cursor-pointer mx-4">
+                            <div className="bg-[#FEEFD0] p-3 pt-[14px] rounded-full cursor-pointer mx-4">
                                 <HiOutlineShoppingBag className='text-xs sm:text-base text-gray-700' />
                             </div>
                         </div>
@@ -65,7 +71,18 @@ const Navbar = (props) => {
                 </div>
 
                 <hr className="bg-black h-[2px] opacity-10" />
+                <div className="hidden lg:block">
+                <div id="ShopSlider" className={`${isShopSlider ? "lg:hidden" : ""}   border-[#96AE01] border-t-2 w-[88%] mx-auto`}>
+                   <div className="grid grid-cols-4  px-8 py-12  shadow-md rounded-b-md  bg-white">
+                    <div className="">Shop1</div>
+                    <div className="">Shop2</div>
+                    <div className="">Shop3</div>
+                    <div className="">Shop4</div>
+                   </div>
+                </div>
+                </div>
             </section>
+
         </>
     )
 }
