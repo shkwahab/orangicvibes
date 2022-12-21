fdf3cb38644c5abcfad7df2c0905aeab13b56dd0
shkwahab <wahab209266@gmail.com>
2022-12-21 22:19:33 +0500

    footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,9 +1,78 @@
 import React from 'react'
+import {CiDeliveryTruck,CiDiscount1} from "react-icons/ci"
+import {MdOutlinePayment} from "react-icons/md"
+import {RiMessage2Line} from "react-icons/ri"
+import {FaRegListAlt} from "react-icons/fa"
+
 
 const Footer = () => {
     return (
         <>
-        <div className="h-[600px] py-40  bg-footer bg-no-repeat"></div>
+        <div className="h-[600px] py-40  bg-footer bg-no-repeat">
+            <div className="container">
+                <div id="services" className="flex justify-center">
+                    <div className=" font-Jost text-center">
+                        <div className="flex justify-center">
+                        <CiDeliveryTruck className='text-4xl text-white'/>
+                        </div>
+                        <div className=' text-white uppercase text-sm tracking-wide font-medium'>
+                            Fast Delivery
+                        </div>
+                        <div className='  text-gray-300'>
+                        Across West & East India
+                        </div>
+                    </div>
+                    <div className="mx-10 font-Jost  text-center">
+                        <div className="flex justify-center">
+                        <MdOutlinePayment className='text-4xl text-white'/>
+                        </div>
+                        <div className=' text-white uppercase text-sm tracking-wide font-medium'>
+                            Safe Payments
+                        </div>
+                        <div className=' text-gray-300'>
+                        100% Secure Payment
+
+                        </div>
+                    </div>
+                    
+                    <div className=" font-Jost  text-center">
+                        <div className="flex justify-center">
+                        <CiDiscount1 className='text-4xl text-white'/>
+                        </div>
+                        <div className=' text-white uppercase text-sm tracking-wide font-medium'>
+                        ONLINE DISCOUNT
+                        </div>
+                        <div className=' text-gray-300'>
+                        Add Multi-buy Discount
+                        </div>
+                    </div>
+
+                    <div className="mx-10 font-Jost  text-center">
+                        <div className="flex justify-center">
+                        <RiMessage2Line className='text-4xl text-white'/>
+                        </div>
+                        <div className=' text-white uppercase text-sm tracking-wide font-medium'>
+                        HELP CENTER
+                        </div>
+                        <div className=' text-gray-300'>
+                        Dedicated 24/7 Support
+                        </div>
+                    </div>
+
+                    <div className=" font-Jost  text-center">
+                        <div className="flex justify-center">
+                        <FaRegListAlt className='text-4xl text-white'/>
+                        </div>
+                        <div className=' text-white uppercase text-sm tracking-wide font-medium'>
+                        CURATED ITEMS
+                        </div>
+                        <div className=' text-gray-300 '>
+                        From Handpicked Sellers
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
         </>
     )
 }
